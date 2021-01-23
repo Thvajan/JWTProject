c14e0bc8150b50f0dc4b386c9c2aa58b60cb0b35
srive <sri.venkatraghavan@gmail.com>
2021-01-23 12:37:32 +0530

    Implemented JWT

--- src/main/java/com/example/JWTProject/MySecurityConfig.java
+++ src/main/java/com/example/JWTProject/MySecurityConfig.java
@@ -2,12 +2,17 @@ package com.example.JWTProject;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.example.JWTProject.filter.JwtRequestFilter;
 import com.example.JWTProject.service.MyUserDetailsService;
 
 @EnableWebSecurity
@@ -15,6 +20,9 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private MyUserDetailsService userDetailsService;
+
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -25,4 +33,18 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 	public PasswordEncoder passwordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
 	}
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf().disable().authorizeRequests().antMatchers("/authenticate").permitAll().anyRequest()
+				.authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+	}
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
 }
--- src/main/java/com/example/JWTProject/controller/HomeController.java
+++ src/main/java/com/example/JWTProject/controller/HomeController.java
@@ -1,14 +1,49 @@
 package com.example.JWTProject.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.JWTProject.jwt.JwtUtil;
+import com.example.JWTProject.model.AuthenticationRequest;
+import com.example.JWTProject.model.AuthenticationResponse;
+import com.example.JWTProject.service.MyUserDetailsService;
 
 @RestController
 public class HomeController {
 
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private MyUserDetailsService userDetailsService;
+
+	@Autowired
+	private JwtUtil jwtUtil;
+
 	@GetMapping("/")
 	public String home() {
 		return "Welcome Home";
+	}
+
+	@PostMapping("/authenticate")
+	public ResponseEntity<?> authenticateUser(@RequestBody AuthenticationRequest request) throws Exception {
+		try {
+			authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+		} catch (BadCredentialsException e) {
+			throw new Exception("Incorrect credentials", e);
+		}
+		UserDetails userDetails = userDetailsService.loadUserByUsername(request.getUsername());
+		String jwt = jwtUtil.generateToken(userDetails);
+		return ResponseEntity.ok(new AuthenticationResponse(jwt));
 	}
 
 }
